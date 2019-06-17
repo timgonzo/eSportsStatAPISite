96319b23e72271f50d27d91be1b8b61e598281a3
Tim Gonzalez <tvgonz@gmail.com>
2019-06-17 00:07:05 -0700

    Began working on scraper console app

---
+++ eSportsScraper/Scraper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace eSportsScraper
+{
+    public class Scraper
+    {
+        //private ObservableCollection<EntryModel> _entries = new ObservableCollection<EntryModel>();
+
+        //public ObservableCollection<EntryModel> Entries
+        //{
+        //    get { return _entries; }
+        //    set { _entries = value; }
+        //}
+
+        //public void ScrapeData(string page)
+        //{
+        //    var web = new HtmlWeb();
+        //    var doc = web.Load(page);
+
+        //    var Results = doc.DocumentNode.SelectNodes("//*[@class = 'results-all']");
+        //}
+
+    }
+}
